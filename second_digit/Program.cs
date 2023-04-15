74929c203b86d8f53fe6021587b3c9c829274bf0
Lydok90 <lyudaharutyunyan@gmail.com>
2023-04-15 23:40:09 +0400

    second digit

---
+++ second_digit/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.*/
+
+Console.WriteLine("Еnter a three-digit number ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 100 || number >= 1000)
+Console.WriteLine("Please, enter a three-digit number ");
+else 
+{
+    int second_digit = number / 10 % 10;
+    Console.WriteLine("Second digit of nuumber = " + second_digit);
+}
+
