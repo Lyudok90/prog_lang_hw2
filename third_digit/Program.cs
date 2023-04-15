3b3ee67520ea852e91c2bae0fd2e6934644c2ad2
Lydok90 <lyudaharutyunyan@gmail.com>
2023-04-16 00:17:55 +0400

    third digit

---
+++ third_digit/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного
+ числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Еnter a number ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 100) 
+Console.WriteLine("Third digit doesn't exist ");  
+
+while(number > 999) 
+{
+    number /= 10;
+        
+}
+
+int third_digit = number % 10;
+Console.WriteLine("Third digit of number = " + third_digit);
+ 
